7ba035e842ccc8b5030e3abbf3bf4156a9541a95
Lzc <20790866@qq.com>
2020-01-23 10:48:51 +0800

    show github name after login

--- src/main/java/com/lzc/community/controller/AuthorizeController.java
+++ src/main/java/com/lzc/community/controller/AuthorizeController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @Controller
@@ -26,7 +27,8 @@ public class AuthorizeController {
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state) throws IOException {
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -35,8 +37,15 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        System.out.println("This is a test for tortoise git commit");
-        return "index";
+        if (user != null) {
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+            //登陆成功，写cookie和session
+
+        } else {
+            // 登陆失败，重新登陆
+            return "redirect:/";
+        }
+
     }
 }
